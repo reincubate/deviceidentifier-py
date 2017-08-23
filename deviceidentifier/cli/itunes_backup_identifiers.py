b76c8f94a4910e763e5aff32edea31a8ddd6455b
Aidan Fitzpatrick <aidan@reincubate.com>
2017-08-23 12:14:34 +0100

    Updated type names.

--- deviceidentifier/cli/itunes_backup_identifiers.py
+++ deviceidentifier/cli/itunes_backup_identifiers.py
@@ -7,7 +7,7 @@ def main():
     # Look at what we can get with the identifiers from an iTunes backup.
 
     if len(sys.argv) != 6:
-        print 'Usage: provide an identifier, a serial number, a model, an IMEI and an ICCID'
+        print 'Usage: provide an identifier, a serial number, an internal code, an IMEI and an ICCID'
         print 'Try: iPhone3,1 5K31926NDZZ N90AP 358686054544170 8965880812100011146'
         exit(-1)
 
@@ -18,7 +18,7 @@ def main():
 
     identifierCode = sys.argv[1:][0]
     serialNumber = sys.argv[1:][1]
-    modelCode = sys.argv[1:][2]
+    internalCode = sys.argv[1:][2]
     imeiCode = sys.argv[1:][3]
     iccidCode = sys.argv[1:][4]
 
